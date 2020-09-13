46dcde2b73b8339652518542f5b9fe1a63e87735
govind <govindmalaghan@gmail.com>
2020-09-13 23:53:52 +0530

    no message

---
+++ MovieServer.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request,session,jsonify
+
+app = Flask(__name__)
+app.secret_key = 'szPnSXYHxNkMKDUEE6FbYfCM'
+
+@app.route("/",methods=['GET','POST'])
+def home():
+  if request.method == 'POST':
+    print(request.form)
+    return jsonify(request.form.to_dict())
+  else:
+    return render_template('horizontal_cards.html')
+
+
+if __name__=="__main__":
+  app.run(debug=True)
