8eba8cb1ae545e6670385011a4fe3334dc8059bb
govind <govindmalaghan@gmail.com>
2020-09-13 09:43:13 +0530

    no message

--- test/cards/FormHandler.js
+++ test/cards/FormHandler.js
@@ -4,3 +4,13 @@ function AddPost()
     var body = document.getElementById("body")
     body.insertBefore(document.getElementById("movie").cloneNode(true), parent.firstChild);
 }
+
+function upload()
+{
+    var reader = new FileReader();
+    file = $("#UpImage")[0].files[0];
+    reader.onload = function(oFREvent){
+        document.getElementById("NewPoster").src = oFREvent.target.result;
+    };
+    reader.readAsDataURL(file);
+}
